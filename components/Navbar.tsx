c65f90f713c78534eaf2089e75af928560a27726
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-10-24 13:17:58 +0530

    Some basic things

---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className="flex justify-end">
+      <div className="flex gap-8">
+        <button>Home</button>
+        <button>Projects</button>
+        <button>Info</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
