aa06abb65e1aaa8dcb550841da06fb83a3c6b66e
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-10-24 13:38:37 +0530

    Navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,43 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+
 const Navbar = () => {
+  const pathname = usePathname();
+  const routes = useMemo(
+    () => [
+      {
+        label: "About",
+        href: "/",
+        active: pathname === "/",
+      },
+      {
+        label: "Projects",
+        href: "/projects",
+        active: pathname === "/projects",
+      },
+      { label: "Info", href: "/info", active: pathname === "/info" },
+    ],
+    [pathname]
+  );
+
   return (
     <div className="flex justify-end">
       <div className="flex gap-8">
-        <button>Home</button>
-        <button>Projects</button>
-        <button>Info</button>
+        {routes.map((item) => (
+          <Link
+            key={item.label}
+            href={item.href}
+            className={
+              item.active
+                ? "text-primary-color sub-font font-bold"
+                : "text-secondary-color sub-font"
+            }
+          >
+            {item.label}
+          </Link>
+        ))}
       </div>
     </div>
   );
