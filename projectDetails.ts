5a4a42f7b03a1947127dd7e5f8e7eefaaa44ca6f
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-10-27 13:13:46 +0530

    copleted project details

--- projectDetails.ts
+++ projectDetails.ts
@@ -1,17 +1,132 @@
 const projectDetails = [
   {
     name: "Spotify Clone",
+    type: "Full-Stack",
     duration: "July23-Oct23",
     tech: "NextJs, ReactJs, TypeScript, Tailwind, PostgreSQL, Supabase, Stripe, ShadCN/UI,...",
-    description: "",
+    description:
+      "A fully functioning Spotify Clone with subscriptions, liked songs, and many more.",
     image: [
-      "public/spotify/without-login.png",
-      "public/spotify/login-modal.png",
-      "public/spotify/login-with-songplayer.png",
-      "public/spotify/mobile-view.png",
-      "public/spotify/subscribe.png",
+      "public/projects-images/spotify-clone-images/without-login.png",
+      "public/projects-images/spotify-clone-images/login-modal.png",
+      "public/projects-images/spotify-clone-images/login-with-songplayer.png",
+      "public/projects-images/spotify-clone-images/mobile-view.png",
+      "public/projects-images/spotify-clone-images/subscribe.png",
     ],
     websiteLink: "https://spotify-clone-one-fawn.vercel.app/",
     gitHubLink: "https://github.com/HarshSikriwal/spotify-clone",
+  },
+  {
+    name: "Neeli Chidiya",
+    type: "Full-Stack",
+    duration: "June23-July23",
+    tech: "NextJs, ReactJs, TypeScript, Tailwind, PostgreSQL, Supabase",
+    description:
+      "A minimalist platform for real-time communication, allowing users to write, like, and interact with each other.",
+    image: [
+      "public/projects-images/neeli-chidya-images/neelicheediya-page-with-login.png",
+      "public/projects-images/neeli-chidya-images/neelicheediya-page-without-login.png",
+    ],
+    websiteLink: "https://neeli-chidiya.vercel.app/",
+    gitHubLink: "https://github.com/HarshSikriwal/NeeliChidiya",
+  },
+  {
+    name: "Github Finder",
+    type: "API Integration",
+    duration: "June23",
+    tech: "ReactJs, JavaScript, Tailwind, DaisyUI, GitHubAPI, REST",
+    description:
+      "A web app that enables users to easily search for any GitHub profile and displays comprehensive GitHub details in a user-friendly interface, utilizing the GitHub API.",
+    image: [
+      "public/projects-images/github-finder-images/home-page.png",
+      "public/projects-images/github-finder-images/search-user.png",
+      "public/projects-images/github-finder-images/user-details.png",
+    ],
+    websiteLink: "https://github-finder-git-main-harshsikriwal.vercel.app/",
+    gitHubLink: "https://github.com/HarshSikriwal/GithubFinder",
+  },
+  {
+    name: "Link Copier",
+    type: "Chrome Extension",
+    duration: "May23",
+    tech: "JavaScript, Tailwind, HTML, CSS",
+    description:
+      "A chrome extension to save and annotate important links for later purpose.",
+    image: [
+      "public/projects-images/link-copier-images/preview.png",
+      "public/projects-images/link-copier-images/with-tab.png",
+      "public/projects-images/link-copier-images/deletall-confirmation.png",
+    ],
+    gitHubLink: "https://github.com/HarshSikriwal/LINK-COPIER",
+  },
+  {
+    name: "Company Landing Page: BlockFlow",
+    type: "Front-End",
+    duration: "Aug23",
+    tech: "ReactJs, TypeScript, Tailwind, Framer-Motion",
+    description:
+      "A landing page for BlockFlow having seamlessly transitions between light and dark themes for a dynamic user experience.",
+    image: [
+      "public/projects-images/blockflow-images/light-theme.png",
+      "public/projects-images/blockflow-images/dark-theme.png",
+    ],
+    websiteLink: "https://blockflow-assignment.vercel.app/",
+    gitHubLink: "https://github.com/HarshSikriwal/blockflow-assignment",
+  },
+  {
+    name: "Premium Page: Grammarly",
+    type: "Front-End",
+    duration: "Nov22",
+    tech: "React, NextJs, Tailwind, TypeScript,...",
+    description:
+      "Premium Page for grammarly created as a assignmentfeaturing sleek design and captivating animations for a engaging user experience",
+    image: [
+      "public/projects-images/grammarly-premium-page/one.png",
+      "public/projects-images/grammarly-premium-page/two.png",
+      "public/projects-images/grammarly-premium-page/three.png",
+      "public/projects-images/grammarly-premium-page/four.png",
+      "public/projects-images/grammarly-premium-page/more-details.png",
+    ],
+    websiteLink: "https://grammarly-premium-page.vercel.app/",
+    gitHubLink: "https://github.com/HarshSikriwal/Grammarly-Premium-Page",
+  },
+  {
+    name: "Product Page: Pepsi",
+    type: "Front-End",
+    duration: "Nov22",
+    tech: "React, Vite, Bootstrap, TypeScript",
+    description:
+      "Recreated a Pepsi product design from Behance into a full functioning web page.",
+    image: [
+      "public/projects-images/pepsi-ui/page-one.png",
+      "public/projects-images/pepsi-ui/page-two.png",
+      "public/projects-images/pepsi-ui/page-three.png",
+    ],
+    websiteLink: "https://pepsi-ui-rosy.vercel.app/",
+    gitHubLink: "https://github.com/HarshSikriwal/PepsiUi",
+  },
+  {
+    name: "Apna Ghar",
+    type: "Full-Stack",
+    duration: "June23-Ongoing",
+    tech: "React, Vite, Tailwind, DaisyUI, React-Router, Authentication, Firebase",
+    description:
+      "Introducing 'Apna Ghar', a web application where users can effortlessly list properties for rent or sale and engage in conversations with potential buyers",
+    image: [
+      "public/projects-images/apna-ghar-images/explore.png",
+      "public/projects-images/apna-ghar-images/account.png",
+      "public/projects-images/apna-ghar-images/offers.png",
+    ],
+    gitHubLink: "https://github.com/HarshSikriwal/Apna-Ghar",
+  },
+  {
+    name: "BuzzOut",
+    type: "Full-Stack",
+    duration: "Aug23-Ongoing",
+    tech: "NextJs, ReactJs, TypeScript, Tailwind, shadcn/ui, PostgreSQL, Supabase",
+    description:
+      "Buzzout, a lighthearted website, offers a collection of random games to entertain and bring laughter to users.",
+    image: ["public/projects-images/buzzout-images/home.png"],
+    gitHubLink: "https://github.com/HarshSikriwal/Buzz-Out",
   },
 ];
