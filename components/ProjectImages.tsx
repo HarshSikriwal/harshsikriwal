89fe549c141b2ff2918873fdda07f86ab811aec4
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-11-06 17:09:39 +0530

    Project description on hover

--- components/ProjectImages.tsx
+++ components/ProjectImages.tsx
@@ -3,6 +3,7 @@ import { projectDetails } from "@/projectDetails";
 import { motion, AnimatePresence } from "framer-motion";
 import { ChevronLeft, ChevronRight, Circle } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { useState, useEffect, useContext } from "react";
 
 const ProjectImages = ({
@@ -39,13 +40,25 @@ const ProjectImages = ({
     <AnimatePresence mode="popLayout" initial={false}>
       <motion.div
         key={currentIndex}
-        className="w-full h-full relative"
+        className="w-full h-full flex flex-col justify-center items-center relative group"
         initial={left ? "enterLeft" : "enterRight"}
         animate={{ opacity: 1, x: "0" }}
         exit={left ? "exitRight" : "exitLeft"}
         transition={{ duration: 0.5, type: "spring", bounce: 0 }}
         variants={directionVariants}
       >
+        <div className="h-full w-full flex flex-col px-8 py-24 gap-6 text-2xl  z-[200] bg-black bg-opacity-90 invisible group-hover:visible">
+          <p className="w-2/3">{myProject?.description}</p>
+          <p className="text-sm">{myProject?.tech}</p>
+          {myProject?.websiteLink && (
+            <Link
+              href={myProject?.websiteLink}
+              className="border-2 rounded-md border-secondary-color text-secondary-color hover:text-primary-color hover:scale-105 hover:border-primary-color w-28"
+            >
+              Live Link
+            </Link>
+          )}
+        </div>
         <Image
           src={myProject?.images[currentIndex]!}
           alt="image"
