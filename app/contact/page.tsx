fc8d7b88d74285d3efac48cdd5c11ad2fba1a922
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-11-15 16:42:20 +0530

    Created form for email content

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const contact = () => {
-  return <div></div>;
+const contact = async () => {
+  return (
+    <div className="h-full w-full flex justify-center">
+      <form className="flex flex-col h-full w-2/3 border-2 rounded-md py-4 px-3">
+        <label htmlFor="email" className="text-primary-color mb-2">
+          Your Email
+        </label>
+        <input
+          name="email"
+          placeholder="Enter your email"
+          className="bg-transparent border-b-2 pb-1 focus:outline-none placeholder:text-secondary-color placeholder:text-sm"
+        />
+        <label className="flex flex-col gap-2 mt-12">
+          <span className="font-medium text-gray-500 text-sm">Message</span>
+          <textarea
+            name="message"
+            className="w-full bg-transparent focus:outline-none text-2xl  custom-textarea"
+            rows={6}
+            spellCheck={false}
+          ></textarea>
+        </label>
+      </form>
+    </div>
+  );
 };
 
 export default contact;
