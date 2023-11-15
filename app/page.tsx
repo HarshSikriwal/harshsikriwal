364941c8bf2c44a0728b182f3fb82cd3b3b3b0b8
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-11-15 15:06:00 +0530

    Created Contact Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,24 @@ import Header from "@/components/Header";
 import Image from "next/image";
 import RoundImage from "../public/Ellipse1.png";
 import MyPhoto from "../public/myphoto.png";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="min-h-[75vh] flex justify-between items-center">
       <div className="flex flex-col gap-8">
         <Header className="name-main" />
-        <div>
+        <div className="">
           <p className="w-2/3 text-2xl font-thin text-primary-color mb-10">
             {`I'm a passionate web developer and recent graduate who thrives on the
           fields of football and Hindi poetry. I live by the motto: 'Life is an
           adventure waiting to be explored.' Also trying to be a bookworm cause
           it sounds geeky.`}
           </p>
-          <button className="rounded-xl border border-secondary-color px-8 py-3 text-secondary-color hover:scale-[0.95] hover:text-white hover:border-primary-color active:scale-90">
-            Get in Touch
+          <button className="border-2 px-4 py-2 border-secondary-color text-secondary-color rounded-lg hover:scale-105 active:scale-95 hover:border-primary-color hover:text-primary-color ">
+            <Link href={"/contact"} className="w-full">
+              Get in Touch
+            </Link>
           </button>
         </div>
       </div>
