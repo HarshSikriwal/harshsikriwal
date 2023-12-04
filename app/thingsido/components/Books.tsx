d89623cf48db0f1aea0cc5cbd8d2fa2e12223ff8
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-12-04 17:07:10 +0530

    added a new design

--- app/thingsido/components/Books.tsx
+++ app/thingsido/components/Books.tsx
@@ -1,17 +1,29 @@
+"use client";
 import React from "react";
 import BooksImage from "./BooksImage";
+import { motion } from "framer-motion";
 
 const Books = () => {
   return (
     <div className="w-full h-2/3 flex relative">
-      <div className="bg-primary-color h-1/5 self-center w-full">
+      <motion.div
+        className="bg-primary-color h-1/5 self-center w-full"
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 0.2 }}
+        transition={{ duration: 1 }}
+      >
         <p className="text-black justify-center text-4xl font-bold h-full w-full flex items-center">
           Books I have read or reading
         </p>
-      </div>
-      <div className="absolute h-full w-full flex px-4 items-center">
+      </motion.div>
+      <motion.div
+        className="absolute h-full w-full flex px-4 items-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
         <BooksImage className="" />
-      </div>
+      </motion.div>
     </div>
   );
 };
--- app/thingsido/components/BooksImage.tsx
+++ app/thingsido/components/BooksImage.tsx
@@ -1,14 +1,35 @@
+"use client";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import React from "react";
 
 let images = ["/books/freakonomics.png", "/books/thinking_fast_and_slow.png"];
 const BooksImage = ({ className }: { className: string }) => {
+  const fadeVariants = {
+    initial: { opacity: 0 },
+    animate: (index: number) => ({
+      opacity: 1,
+
+      transition: {
+        delay: 0.2 * index,
+      },
+    }),
+  };
+
   return (
     <div className="flex gap-10">
-      {images.map((image: string) => (
-        <div key={image} className="">
+      {images.map((image, index) => (
+        <motion.div
+          key={image}
+          transition={{ duration: 0.5 }}
+          variants={fadeVariants}
+          initial="initial"
+          whileInView="animate"
+          custom={index}
+          viewport={{ once: true }}
+        >
           <Image src={image} height={300} width={200} alt="book image" />
-        </div>
+        </motion.div>
       ))}
     </div>
   );
