703b3b5c7fff627ffe5c14e2ea14903685dcd6ef
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-12-07 14:29:12 +0530

    added project details

--- components/ProjectAboutForMobile.tsx
+++ components/ProjectAboutForMobile.tsx
@@ -1,8 +1,18 @@
 import { projectDetails } from "@/projectDetails";
 import React from "react";
+import { SkillBadge } from "./SkillBadge";
 
-const ProjectAboutForMobile = ({}: { project: (typeof projectDetails)[0] }) => {
-  return <div>Details</div>;
+const ProjectAboutForMobile = ({
+  project,
+}: {
+  project: (typeof projectDetails)[0];
+}) => {
+  return (
+    <div className="flex flex-col gap-4 my-4">
+      <p className="text-secondary-color text-2xl">{project.description}</p>
+      <SkillBadge skills={project.tech} />
+    </div>
+  );
 };
 
 export default ProjectAboutForMobile;
--- components/ProjectForMobile.tsx
+++ components/ProjectForMobile.tsx
@@ -5,12 +5,13 @@ import ProjectAboutForMobile from "./ProjectAboutForMobile";
 
 const ProjectForMobile = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       {projectDetails.map((project, index) => (
         <div key={project.name} className="w-full  overflow-hidden">
-          <p className="text-4xl my-4">{project.name}</p>
+          <p className="text-3xl font-bold my-4">{project.name}</p>
           <ProjectImagesForMobile images={project.images} />
           <ProjectAboutForMobile project={project} />
+          <Links live={project.websiteLink} github={project.gitHubLink} />
         </div>
       ))}
     </div>
