ba251abbc6cfd763cfe83df7dc4fadc5846470a9
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-12-08 13:05:46 +0530

    done, with the contact page

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -14,7 +14,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed bottom-0  z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-2 sm:top-auto sm:flex-col md:max-w-[420px]",
+      "fixed bottom-0  z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 md:right-2 sm:top-auto sm:flex-col md:max-w-[420px]",
       className
     )}
     {...props}
