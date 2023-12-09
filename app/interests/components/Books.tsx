93a722d9231394e9d43f9198b8bc06e6eddd497f
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-12-09 12:31:43 +0530

    now going to add poems

--- app/interests/components/Books.tsx
+++ app/interests/components/Books.tsx
@@ -5,15 +5,15 @@ import { motion } from "framer-motion";
 
 const Books = () => {
   return (
-    <div className="w-full h-full md:h-2/3 flex flex-col md:flex-row relative max-md:mt-16">
+    <div className="w-full h-full md:h-2/3 flex flex-col gap-16 md:flex-row relative max-md:mt-16">
       <motion.div
-        className="bg-primary-color h-1/6 md:h-1/5 self-center w-full"
+        className="bg-primary-color h-1/5 self-center w-full"
         initial={{ opacity: 1 }}
         whileInView={{ opacity: 0.2 }}
         transition={{ duration: 0.5 }}
         viewport={{ once: true, amount: 1 }}
       >
-        <p className="text-black justify-center text-2xl md:text-4xl font-bold h-full w-full flex items-center">
+        <p className="text-black justify-center text-3xl py-1 md:text-4xl font-bold h-full w-full flex items-center">
           Readings
         </p>
       </motion.div>
--- app/interests/components/Football.tsx
+++ app/interests/components/Football.tsx
@@ -6,15 +6,15 @@ import FootballImages from "./FootballImages";
 
 const Football = () => {
   return (
-    <div className="w-full grow h-full md:h-2/3 flex flex-col gap-12 md:flex-row relative">
+    <div className="w-full grow h-full md:h-2/3 flex flex-col gap-16 md:flex-row relative">
       <motion.div
-        className="bg-primary-color h-1/6 md:h-1/5 self-center w-full"
+        className="bg-primary-color h-1/5 self-center w-full"
         initial={{ opacity: 1 }}
         whileInView={{ opacity: 0.2 }}
         transition={{ duration: 0.5 }}
         viewport={{ once: true, amount: 1 }}
       >
-        <p className="text-black justify-center text-2xl md:text-4xl font-bold h-full w-full flex items-center">
+        <p className="text-black justify-center text-3xl py-1 md:text-4xl font-bold h-full w-full flex items-center">
           Football
         </p>
       </motion.div>
