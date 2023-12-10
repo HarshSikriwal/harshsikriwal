c06148d041d35497d920a0c52af9fc4482218704
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-12-10 14:06:57 +0530

    added more about me

---
+++ components/MoreAboutMe.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const MoreAboutMe = () => {
+  return (
+    <div className="text-lg lg:text-2xl font-semibold">
+      <p>
+        {`
+            In 2019, started my college journey as a Electronics Student, knowing only a bit about HTML. Also, when preparing for Mains and Advance you only study Maths, Physics, Chemistry, programming language was not even in the glossary section of my studies. Even coming from a place like Bihar, being good in Powerpoint only, makes you feel like a guy who knows computer well.
+            However, everything changed when I stepped into my first computer lab. Terms like C, Python, List, Array, and Abstract data began to overwhelm me. Despite grasping their meanings, I struggled and completed my first year with a D grade in Computer Programming. Despite the challenges in my E.C.E. courses, I found a liking for the subject, but then the COVID-19 pandemic struck.
+            Now as eduaction shifted online, even E.C.E. became more about learning different Programming languages like, MATLAB, VHDL, etc.
+            While managing to maintain a GPA around 7.8, I encountered the toughest challenge yet - securing placements during a recession. Realizing that aiming for a package above 10LPA required proficiency in DSA, I dedicated myself to mastering Python for this purpose.
+            Then, the realm of web development entered my sphere, leading me to study JavaScript through the book 'Eloquent JavaScript.' However, a personal struggle arose between Python and JavaScript. Balancing these, as companies started recruiting from my college, I faced new challenges requiring knowledge in Database Management System, Operating Systems, Computer Networks, and Object-Oriented Programming.
+
+            Now at this point things started going south for me, neither I was concentrated on web development nor on the things I need to study for placement. 
+            As my fourth year commenced, I realized that concentrating on one path at a time was the solution I had been neglecting for too long.
+
+           This realization led me to make a significant decision - to focus on completing JavaScript (though it's still ongoing). I developed my first HTML, CSS, JavaScript project, 'BLACKJACK,' followed by a Chrome extension named 'Link Copier' (available in the project section) 
+            
+           Now when I felt, Okay! now I am a developer, 'React' said Hi to me, Created several projects in React and Tailwind. Then 'NextJs' joined the party with two friend who appears like rivals but if you somehow manage to make them friends, things will be changed for you for forever: 'FRONTEND AND BACKEND'. This leads to my two Full Stack Project 'Neeli Chidiya and Spotify Clone'.
+           While I'm still more inclined towards Frontend, I refuse to halt my journey here. Web Development initially sparked hope within me, and now I'm striving to integrate it into my identity. I pledge to wake up every day, commit to GitHub, continuously study JavaScript, and undertake new projects to explore additional tech stacks.
+        
+        `}
+      </p>
+    </div>
+  );
+};
+
+export default MoreAboutMe;
