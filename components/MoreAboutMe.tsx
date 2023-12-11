56a76ea419ea851605a23d39dbe2df579944ec9b
HarshSikriwal <19uec030@lnmiit.ac.in>
2023-12-11 14:12:45 +0530

    modified more about me

--- components/MoreAboutMe.tsx
+++ components/MoreAboutMe.tsx
@@ -5,19 +5,35 @@ const MoreAboutMe = () => {
     <div className="text-lg lg:text-2xl font-semibold">
       <p>
         {`
-            In 2019, started my college journey as a Electronics Student, knowing only a bit about HTML. Also, when preparing for Mains and Advance you only study Maths, Physics, Chemistry, programming language was not even in the glossary section of my studies. Even coming from a place like Bihar, being good in Powerpoint only, makes you feel like a guy who knows computer well.
-            However, everything changed when I stepped into my first computer lab. Terms like C, Python, List, Array, and Abstract data began to overwhelm me. Despite grasping their meanings, I struggled and completed my first year with a D grade in Computer Programming. Despite the challenges in my E.C.E. courses, I found a liking for the subject, but then the COVID-19 pandemic struck.
-            Now as eduaction shifted online, even E.C.E. became more about learning different Programming languages like, MATLAB, VHDL, etc.
-            While managing to maintain a GPA around 7.8, I encountered the toughest challenge yet - securing placements during a recession. Realizing that aiming for a package above 10LPA required proficiency in DSA, I dedicated myself to mastering Python for this purpose.
-            Then, the realm of web development entered my sphere, leading me to study JavaScript through the book 'Eloquent JavaScript.' However, a personal struggle arose between Python and JavaScript. Balancing these, as companies started recruiting from my college, I faced new challenges requiring knowledge in Database Management System, Operating Systems, Computer Networks, and Object-Oriented Programming.
+            The journey of coding is something which I don't wanted in the beginning but now it becomes a major part of my identity.
+            When preparing for Mains and Advance only studied Maths, Physics and Chemistry, unaware about Computer Programming's power.
+            In 2019, started my college journey as a Electronics Student, knowing only a bit about HTML. Coming from a small town, being good in Powerpoint only, makes you feel like a guy who knows computer well.
+            However, everything changed when I stepped into my first computer lab. Terms like C, Python, List, Array, and Abstract data began to overwhelm me. Despite grasping their meanings, I struggled and completed my first year with a D grade in Computer Programming.
+            Also E.C.E. being itself a vast course, has its own challenges, but somehow I found a liking for this subject maybe due to multiple labs and experiments. But then COVID-19 pademic happened.
+            Eduaction shifted online, even E.C.E. became more about learning different Programming languages like, MATLAB, VHDL, etc.
+            At this point struggling again with codes I reached to 3rd Year somehow maintaining the GPA around 7.8.
+            Then I encountered the toughest challenge - securing placements during a recession. Realizing that aiming for a package above 10LPA required proficiency in DSA, I chose Python as my programming language to acheive so.
+            Studying E.C.E. subjects now became only a compulsion for having good grades to sit in high G.P.A. requiring companies.
+            This is the position when I was not true to myself, just doing things what others were defining for me, not realizing that I was losing myself. One thing I know for sure, that I was not happy.
+            Then saw one of my closest person creating websites. Changing background colors adding images  was enough for me to study this.
+            Things from outside looks simple but when you start doing it, it became something like peeling onion layer one by one.
 
-            Now at this point things started going south for me, neither I was concentrated on web development nor on the things I need to study for placement. 
-            As my fourth year commenced, I realized that concentrating on one path at a time was the solution I had been neglecting for too long.
+            So, web development came into the picture, leading me to study JavaScript then HTML(again) and CSS.
+            However, a personal struggle arose between Python and JavaScript. Balancing these, as companies started recruiting from my college, I faced new challenges requiring knowledge in Database Management System, Operating Systems, Computer Networks, and Object-Oriented Programming.
+            Now things started going south for me, neither I was concentrated on web development nor on the topics I need to study for placement.
 
-           This realization led me to make a significant decision - to focus on completing JavaScript (though it's still ongoing). I developed my first HTML, CSS, JavaScript project, 'BLACKJACK,' followed by a Chrome extension named 'Link Copier' (available in the project section) 
+            As my fourth year started, I realized that concentrating on one path at a time was the solution I had been neglecting for too long.
+
+           This realization led me to make a significant decision - to focus completely on JavaScript (though it's still ongoing). I developed my first HTML, CSS, JavaScript project, 'BLACKJACK,' followed by a Chrome extension named 'Link Copier' (available in the project section) 
             
-           Now when I felt, Okay! now I am a developer, 'React' said Hi to me, Created several projects in React and Tailwind. Then 'NextJs' joined the party with two friend who appears like rivals but if you somehow manage to make them friends, things will be changed for you for forever: 'FRONTEND AND BACKEND'. This leads to my two Full Stack Project 'Neeli Chidiya and Spotify Clone'.
-           While I'm still more inclined towards Frontend, I refuse to halt my journey here. Web Development initially sparked hope within me, and now I'm striving to integrate it into my identity. I pledge to wake up every day, commit to GitHub, continuously study JavaScript, and undertake new projects to explore additional tech stacks.
+           So, when I felt, Okay! now I am a developer, 'React' said Hi to me, completed a React tutorial Udemy course, created several projects in React and Tailwind.
+          Till now I had not completed any major full-stack project, then 'NextJs' joined the party unleashing server-side rendering, routing, SEO, smooth integration with backend through API routing and many more. Thus created Neeli Chidiya and then the biggest project 'Spotify Clone'.
+
+           While I'm still more inclined towards Frontend, I refuse to halt my journey here. Web Development initially sparked hope within me, and now I'm striving to integrate it into my identity. I will wake up everyday to do more and more GitHub commits, study again and again JavaScript, and undertake new projects to explore additional tech stacks.
+
+           Well, now if I think about it, creating something from scratch makes me happy maybe that was the reason for me to like E.C.E. cause I was doing experiments and looking for answers by myself. 
+          
+           Thanks if you have read till now.
         
         `}
       </p>
